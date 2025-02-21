4072cc8224504602e31a8f060a80d980d737f6b4
jhowgama <jonathan.gama@teltecsolutions.com.br>
2025-02-21 12:03:53 -0400

    add new imgs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/effect-fade';
 import * as echarts from 'echarts';
-import img from 'next/image';
 
 const App: React.FC = () => {
     const [selectedFaq, setSelectedFaq] = useState<number | null>(null);
